0164acd3c6974684fee3f6ab680f8a112b153f1f
tfishbeck95 <tyfishbeck51@gmail.com>
2018-09-26 12:41:50 -0500

    This is the new commit change

--- testInitialize4.py
+++ testInitialize4.py
@@ -6,6 +6,3 @@ Created on Wed Sep 26 10:01:46 2018
 """
 
 print('Hello World this is a test repository')
-print('This is the second commit')
-print('This is the second commit')
-print('This is the second commit')
